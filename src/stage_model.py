b30939d4277cdef1475cd585fa7a6871e588e2e4
phum1901 <phum_1901@hotmail.com>
2023-04-15 00:30:15 +0700

    edit generate func

--- src/stage_model.py
+++ src/stage_model.py
@@ -15,7 +15,9 @@ def save_model_checkpoint_to_torchscript(ckpt_path: str, save_path: str):
 
 
 class StageModel:
-    def __init__(self, model_path):
+    def __init__(self, model_path=None):
+        if model_path is None:
+            model_path = "model.pt"
         self.model = torch.jit.load(model_path)
         self.chars = self.model.chars
         self.seq_length = self.model.seq_length
@@ -25,14 +27,19 @@ class StageModel:
         self.decode = lambda t: "".join([self.itos[i] for i in t])
 
     @torch.no_grad()
-    def predict(self, text: str, max_token=100):
+    def generate(self, text, max_tokens=100, temperature=1.0, top_k=None):
         idx = torch.tensor(self.encode(text), dtype=torch.int64).unsqueeze(
             dim=0
         )  # batch like (b, s, c)
-        for _ in range(max_token):
-            idx_crop = idx[:, -self.seq_length :]  #
-            logits = self.model(idx_crop)
-            logits = logits[:, -1, :]
+        for _ in range(max_tokens):
+            idx_crop = idx[:, -self.seq_length :]
+
+            logits = self.model(idx_crop)  # (b, s, vocab_size)
+            logits = logits[:, -1, :] / temperature  # (b, vocab_size)
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < torch.min(v)] = float("-inf")
+
             probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat([idx, idx_next], dim=1)
